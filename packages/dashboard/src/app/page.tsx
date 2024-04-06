0f7a7cfe116c4f40171c95ccbd8c0fa4873513f0
asim <asimpk.dev@gmail.com>
2024-04-06 15:23:00 +0500

    feat: used wagmi hooks to test fetch blockchain data
    - Implemented Wagmi hooks for data fetching
    - Implemented `useBlock` hook to retrieve blockchain block data (`block`)
    - Utilized `useBlockTransactionCount` hook to fetch transaction count for a block (`transactionCount`)
    - Incorporated `useGasPrice` hook to fetch current gas price from the blockchain (`gasPrice`)

--- packages/dashboard/src/app/page.tsx
+++ packages/dashboard/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-between px-24 py-4">
-       Dashboard  
+    <main className="flex flex-col items-center min-h-screen px-24 py-4">
+      <h2 className="text-2xl font-bold text-gray-800">Dashboard</h2>
     </main>
   );
 }
