8d03eefaab5cc1f47f62e275d087d9a3a898eeb0
asim <asimpk.dev@gmail.com>
2024-04-08 02:21:18 +0500

    feat: added initial responsiveness
    - added responsiveness in content component
    - fixes responsive issues in block page

--- packages/dashboard/src/app/block/[blockNumber]/page.tsx
+++ packages/dashboard/src/app/block/[blockNumber]/page.tsx
@@ -28,87 +28,107 @@ const BlockPage = () => {
                 <h2 className="text-lg font-semibold text-gray-600 ml-2">Block {Number(block?.number)}</h2>
             </div>
 
-
-            
             <h3 className="text-sm font-semibold mt-4 text-gray-600 bg-gray-300 rounded-full px-2 py-1 inline-block">
                 Overview
             </h3>
 
-            <div className="bg-gray-200 my-4 p-4 rounded-lg shadow-lg w-full">
+            <div className="bg-gray-200 my-4 p-4 rounded-lg shadow-lg w-full overflow-auto">
                 <div className="flex flex-col">
                     <div className="mb-4">
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>Block Height:</strong>
                             </p>
-                            <p>{Number(block?.number)}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {Number(block?.number)}
+                            </p>
                         </div>
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>Timestamp:</strong>
                             </p>
-                            <p>{formatTimestamp(Number(block?.timestamp))}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {formatTimestamp(Number(block?.timestamp))}
+                            </p>
                         </div>
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>Transactions:</strong>
                             </p>
-                            <p>{(block?.transactions)?.length}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {(block?.transactions)?.length}
+                            </p>
                         </div>
                     </div>
 
                     <div className="mb-4">
                         <h2 className="text-lg font-semibold mb-2 text-gray-600">Gas Details</h2>
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>Gas Used:</strong>
                             </p>
-                            <p>{Number(block?.gasUsed)}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {Number(block?.gasUsed)}
+                            </p>
                         </div>
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>Gas Limit:</strong>
                             </p>
-                            <p>{Number(block?.gasLimit)}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {Number(block?.gasLimit)}
+                            </p>
                         </div>
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>Base Fee Per Gas:</strong>
                             </p>
-                            <p>{Number(block?.baseFeePerGas)}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {Number(block?.baseFeePerGas)}
+                            </p>
                         </div>
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>Burnt Fees:</strong>
                             </p>
-                            <p>{Number(block?.blobGasUsed)}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {Number(block?.blobGasUsed)}
+                            </p>
                         </div>
                     </div>
                     <div className="mb-4">
                         <h2 className="text-lg font-semibold mb-2 text-gray-600">Additional Information</h2>
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>Parent Hash:</strong>
                             </p>
-                            <p>{block?.parentHash}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {block?.parentHash}
+                            </p>
                         </div>
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>StateRoot:</strong>
                             </p>
-                            <p>{block?.stateRoot}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {block?.stateRoot}
+                            </p>
                         </div>
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>WithdrawalsRoot:</strong>
                             </p>
-                            <p>{block?.withdrawalsRoot}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {block?.withdrawalsRoot}
+                            </p>
                         </div>
-                        <div className="flex">
-                            <p className="w-60 text-gray-700">
+                        <div className="flex flex-wrap">
+                            <p className="w-full md:w-48 lg:w-60 text-gray-700">
                                 <strong>Nonce:</strong>
                             </p>
-                            <p>{block?.nonce}</p>
+                            <p className="w-full md:w-auto text-gray-700">
+                                {block?.nonce}
+                            </p>
                         </div>
                     </div>
                 </div>
